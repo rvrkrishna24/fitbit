bdc07cd89f80b0611e281e8e6532f3bb93154ead
rvrkrishna24 <ramakrishna.rv@gmail.com>
2015-09-08 14:08:38 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ module.exports = {
     db: 'mongodb://localhost/fitbit',
     port: 3000,
     fb: {
-      app_id : '1481154355515850',
-      app_secret : '75f2ca2b2904d40f4c797d78537453d7',
+      app_id : '', //give your app id
+      app_secret : '', //give your fb app secret
       callbackURL :  'http://localhost:3000/auth/facebook/callback',
       feedUrl: 'https://graph.facebook.com/me/feed',
       locationRedirect: 'http://localhost:3000'
     },
     google: {
-      client_id : '536416163748-e4rjm6cbdkscbu4ereeone051gj5pd5b.apps.googleusercontent.com',
-      client_secret : 'AIxVfBkVK93HoXHmo6j4WToO',
+      client_id : '',//Create a client id for your gmail
+      client_secret : '', //Create one for your gmail
       callbackURL :  'http://localhost:3000/auth/google/callback',
       locationRedirect: 'http://localhost:3000'
     },
