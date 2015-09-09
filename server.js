ae16390435ba6c417886a7b3e9fb09b8302e1d77
Rajesh Shegokar <rajeshx.shegokar@intel.com>
2015-09-09 15:50:57 +0530

    replaces express with restify

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var fs = require('fs'),
 	path = require('path'),
-	express = require('express'),
+	restify = require('restify');
 	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	http = require('http'),
-	config = require('./config/config'),
-	session = require('express-session'),
-	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),	
+	config = require('./config/config'),		
 	winston = require('winston');
 
 var passport = require('passport'),
@@ -17,7 +14,10 @@ var user = require('./app/models/user');
 winston.add(winston.transports.File, { filename: 'logallerrors.log' });
 winston.remove(winston.transports.Console);
 
-var app = express();
+var server = restify.createServer({
+  name: 'fitbit',
+  version: '1.0.0'
+});
 
 var connectToDb = function () {
   	var options = { server: { socketOptions: { keepAlive: 1 } } };
@@ -30,30 +30,40 @@ mongoose.connection.on('disconnected', connectToDb);
 
 require('./config/passport')(passport); // pass passport for configuration
 
-//set envs
-app.set('port', process.env.PORT || config.port);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+var userCtrl = require('./app/controllers/user.js'),
+	authenticate = require('./config/authenticate.js');
 
-app.use(session({ secret: 'neededforpassport', resave: true, saveUninitialized: true })); // session secret
-app.use(passport.initialize());
-app.use(passport.session());
+server.get(/.*/, restify.serveStatic({
+  directory: './public/',
+  default: 'views/index.html'
+}));
 
-app.use(express.static(path.join(__dirname, 'public')));
+server.post('/user/register', userCtrl.registerUser);
 
+server.post('/user/login', authenticate.authCheck, userCtrl.loginUser);
 
-// passport config
-passport.use(new LocalStrategy(user.authenticate()));
+server.get('/user/logout', userCtrl.userLogout);
+//server.delete('/user', userCtrl.userDelete); //only for tests
 
-var routes = require('./config/routes');
+//other apis
 
-app.use('/', routes);
+server.post('/users/:uname', authenticate.authCheck, userCtrl.addUserPlans);
+server.put('/users/:uname', authenticate.authCheck, userCtrl.updateUserPlans);
 
-var server = http.createServer(app);
-server.listen(app.get('port'), function(){
-  	console.log('Express server listening on port ' + app.get('port'));
+//server.delete('/users/:uname', authenticate.authCheck, userCtrl.deleteUserWorkout);
+
+server.get('/users/:uname/:day', authenticate.authCheck, userCtrl.getUserDetailsDay);
+
+server.get('/fblogin', passport.authenticate('facebook', { scope : ['email'] }), userCtrl.fb.login );
+server.get('/auth/facebook/callback', passport.authenticate('facebook'), userCtrl.fb.callback);
+
+server.get('/googlelogin', authenticate.authCheck, passport.authenticate('google', { scope : 'email' }), userCtrl.google.login );
+server.get('/auth/google/callback', authenticate.authCheck, passport.authenticate('google'), userCtrl.google.callback);
+
+server.listen(3000, function () {
+  console.log('listening -->' + server.name);
 });
